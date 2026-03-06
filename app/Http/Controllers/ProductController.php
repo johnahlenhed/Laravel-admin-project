0da7c6accbccef73c47252c5cb469d503b4d7bf4
Amin El-Rifai <amin@users.noreply.github.com>
2026-03-07 00:53:27 +0100

    chore: renamed ProductRequest to ProductIndexRequest

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,87 +1,87 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Http\Requests\ProductRequest;
-use App\Http\Requests\ProductStoreRequest;
-use App\Models\Product;
-use App\Models\Category;
+    use App\Http\Requests\ProductIndexRequest;
+    use App\Http\Requests\ProductStoreRequest;
+    use App\Models\Product;
+    use App\Models\Category;
 
-class ProductController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(ProductRequest $request)
+    class ProductController extends Controller
     {
+        /**
+         * Display a listing of the resource.
+         */
+        public function index(ProductIndexRequest $request)
+        {
 
-        $products = Product::query()
-            ->when($request->filled('category'), fn($q) => $q->where('category_id', $request->category))
-            ->when($request->filled('minPrice'), fn($q) => $q->where('price', '>=', $request->minPrice))
-            ->when($request->filled('maxPrice'), fn($q) => $q->where('price', '<=', $request->maxPrice))
-            ->orderBy('price', $request->input('sort', 'asc'))
-            ->orderBy('name', 'asc')
-            ->paginate(10);
+            $products = Product::query()
+                ->when($request->filled('category'), fn($q) => $q->where('category_id', $request->category))
+                ->when($request->filled('minPrice'), fn($q) => $q->where('price', '>=', $request->minPrice))
+                ->when($request->filled('maxPrice'), fn($q) => $q->where('price', '<=', $request->maxPrice))
+                ->orderBy('price', $request->input('sort', 'asc'))
+                ->orderBy('name', 'asc')
+                ->paginate(10);
 
-        return view('products.index', [
-            'products' => $products,
-            'categories' => Category::all(),
-        ]);
+            return view('products.index', [
+                'products' => $products,
+                'categories' => Category::all(),
+            ]);
+        }
+
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            return view('products.create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(ProductStoreRequest $request)
+        {
+            Product::create($request->validated());
+            return redirect()->route('products.index');
+        }
+
+        /**
+         * Display the specified resource.
+         */
+        public function show(string $id)
+        {
+            //
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(Product $product)
+        {
+            return view('products.edit', [
+                'categories' => Category::all(),
+                'product' => $product
+            ]);
+        }
+
+        /**
+         * Update the specified resource in storage.
+         */
+
+        public function update(ProductStoreRequest $request, Product $product)
+        {
+
+            $product->update($request->validated());
+            return redirect()->route('products.index');
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(Product $product)
+        {
+            $product->delete();
+            return redirect()->route('products.index');
+        }
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('products.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(ProductStoreRequest $request)
-    {
-        Product::create($request->validated());
-        return redirect()->route('products.index');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    // public function show(string $id)
-    // {
-    //     //
-    // }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        return view('products.edit', [
-            'categories' => Category::all(),
-            'product' => $product
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-
-    public function update(ProductStoreRequest $request, Product $product)
-    {
-
-        $product->update($request->validated());
-        return redirect()->route('products.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        $product->delete();
-        return redirect()->route('products.index');
-    }
-}
--- app/Http/Requests/ProductIndexRequest.php
+++ app/Http/Requests/ProductIndexRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
 
-class ProductRequest extends FormRequest
+class ProductIndexRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
